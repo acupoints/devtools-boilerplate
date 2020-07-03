c2ea35aedf9bbc81a5e9e2df577c18f1daf4edcf
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-03 16:17:30 +0800

    Update Ubuntu_BaseIDE.sh

--- Ubuntu_BaseIDE.sh
+++ Ubuntu_BaseIDE.sh
@@ -2,13 +2,10 @@
 sudo passwd root
 su root
 
-## Install pyenv
-apt update -y
-apt install -y make build-essential libssl-dev zlib1g-dev \
-libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev\
-libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python-openssl\
-git vim curl
+## Install common tools
+apt install -y git vim curl
 
+## Install pyenv
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
