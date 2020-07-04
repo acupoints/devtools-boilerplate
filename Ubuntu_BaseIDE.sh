f12d4e510430e4e87c086e4579a2ef5f93324438
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-04 08:29:16 +0800

    Update Ubuntu_BaseIDE.sh

--- Ubuntu_BaseIDE.sh
+++ Ubuntu_BaseIDE.sh
@@ -13,10 +13,10 @@ echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi
 exec "$SHELL"
 
 apt update -y
-apt install -y liblzma-dev libffi-dev tk-dev xz-utils libncursesw5-dev libncurses5-dev llvm libsqlite3-dev \
-libreadline-dev libbz2-dev zlib1g-dev libssl-dev build-essential make libffi-dev openssl libssl-dev \
-tk-dev libsqlite3-dev zlib1g-dev libc6-dev libgdbm-dev libncursesw5-dev build-essential \
-python-smbus python-pip python-setuptools python-dev build-essential
+apt install -y build-essential libbz2-dev libc6-dev libffi-dev libgdbm-dev liblzma-dev \
+libncurses5-dev libncursesw5-dev libreadline-dev libsqlite3-dev libssl-dev llvm make \
+openssl python-dev python-pip python-setuptools python-smbus sqlite3 \
+tk-dev xz-utils zlib1g-dev
 
 pyenv install --list
 pyenv install 3.7.6
@@ -24,13 +24,18 @@ pyenv versions
 pyenv global 3.7.6
 
 ## Install ruby
-sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-sudo curl -sSL https://get.rvm.io | sudo bash -s stable
-sudo usermod -a -G rvm `whoami`
+### This method requires turning over the wall
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+# Install only RVM
+\curl -sSL https://get.rvm.io | bash -s stable
+# For installing RVM with default Ruby and Rails in one command, run
+\curl -sSL https://get.rvm.io | bash -s stable --rails
 
-# source ~/.bashrc
-# source ~/.bash_profile
-source /usr/local/rvm/scripts/rvm
+### Recommended installation, no need to turn the wall
+git clone https://github.com/rvm/rvm.git
+cat rvm/binscripts/rvm-installer | sudo bash -s stable
+source ~/.bashrc
+rvm --version
 
 rvm list known
 rvm install 2.6.5
