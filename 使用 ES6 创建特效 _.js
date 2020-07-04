b6f862f3aa3c3dc794ff3c3e2ad40e91235e9911
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-04 11:40:37 +0800

    Create 使用 ES6 创建特效 _.js

---
+++ 使用 ES6 创建特效 _.js
@@ -0,0 +1,40 @@
+/**
+* jQuery UI - v1.12.1 - 2016-09-14
+* 滚动客户区时，导航栏浮动到上层，位置固定在那里
+*/
+(function($) {
+    'use strict';
+    $(function() {
+        $('[data-toggle="tooltip"]').tooltip();
+        $('[data-toggle="popover"]').popover();
+        $('.popover-dismiss').popover({
+            trigger: 'focus'
+        })
+    });
+    function bottomPos(element) {
+        return element.offset().top + element.outerHeight();
+    }
+    $(function() {
+        var promo = $(".js-td-cover");
+        if (!promo.length) {
+            return
+        }
+        var promoOffset = bottomPos(promo);
+        var navbarOffset = $('.js-navbar-scroll').offset().top;
+        var threshold = Math.ceil($('.js-navbar-scroll').outerHeight());
+        if ((promoOffset - navbarOffset) < threshold) {
+            $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
+        }
+        $(window).on('scroll', function() {
+            var navtop = $('.js-navbar-scroll').offset().top - $(window).scrollTop();
+            var promoOffset = bottomPos($('.js-td-cover'));
+            var navbarOffset = $('.js-navbar-scroll').offset().top;
+            if ((promoOffset - navbarOffset) < threshold) {
+                $('.js-navbar-scroll').addClass('navbar-bg-onscroll');
+            } else {
+                $('.js-navbar-scroll').removeClass('navbar-bg-onscroll');
+                $('.js-navbar-scroll').addClass('navbar-bg-onscroll--fade');
+            }
+        });
+    });
+}(jQuery));
