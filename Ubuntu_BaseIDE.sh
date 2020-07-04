506b31421e4fc13b1b0c597d08a8f80d282bd93c
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-04 10:26:50 +0800

    Update Ubuntu_BaseIDE.sh

--- Ubuntu_BaseIDE.sh
+++ Ubuntu_BaseIDE.sh
@@ -34,6 +34,7 @@ gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D69
 ### Recommended installation, no need to turn the wall
 git clone https://github.com/rvm/rvm.git
 cat rvm/binscripts/rvm-installer | sudo bash -s stable
+echo source ~/.rvm/scripts/rvm >> ~/.bashrc
 source ~/.bashrc
 rvm --version
 
