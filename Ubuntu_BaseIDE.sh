b7adf13dbb8790f5fa86e679b15657afbb72bec3
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-04 13:11:19 +0800

    Update Ubuntu_BaseIDE.sh

--- Ubuntu_BaseIDE.sh
+++ Ubuntu_BaseIDE.sh
@@ -97,7 +97,7 @@ ssh -T git@github.com
 ssh -T git@github.com
 {yes}{ENTER}
 
-===
+=== 疑惑 01
 $ ssh -T git@github.com
 Hi acupoints! You've successfully authenticated, but GitHub does not provide shell access.
 
@@ -105,3 +105,13 @@ Hi acupoints! You've successfully authenticated, but GitHub does not provide she
 git:https://github.com
 (删除)_
 
+=== 疑惑 02
+gullies@gullies-VirtualBox:~$ rvm --version
+Warning! PATH is not properly set up, /home/gullies/.rvm/gems/ruby-2.6.5/bin is not at first place.
+### 这个提示在Bash Shell 配置文件中已经说明，导出变量，排在第一
+### 移动一下RVM 变量的位置到文件最后，保存退出即可
+gullies@gullies-VirtualBox:~$ vim ~/.bashrc
+...
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+source /home/gullies/.rvm/scripts/rvm
