284b313d0d047ae77ab103710b7f1cac16695e45
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-06 22:18:39 +0800

    Update Babel 6 的安装与使用 _.sh

--- Babel 6 的安装与使用 _.sh
+++ Babel 6 的安装与使用 _.sh
@@ -1,27 +1,14 @@
-## 参考文档
-## https://babeljs.io/docs/en/6.26.3/babel-preset-es2015
+npm init -y
+npm install -D babel-cli babel-preset-es2015
+echo '{ "presets": ["es2015"] }' > .babelrc
 
-## 简易操作步骤
-## STEP 01
-# 生成package.json
-yarn init -y
+## 配置package.json的scripts.babel
+"babel": "./node_modules/.bin/babel-node index.js"
+npm run babel
 
-## STEP 02
-# 安装 Babel 6
-yarn add -D babel-cli babel-preset-es2015
-
-## STEP 03
-# 配置 Babel 6
+# yarn init -y
+# yarn add -D babel-cli babel-preset-es2015
 # echo '{ "presets": ["es2015"] }' > .babelrc
-echo -e '{\n\t"presets": ["es2015"]\n}' > .babelrc
-
-## STEP 04
-# 运行 Babel 6
-./node_modules/.bin/babel-node index.js
-
-## 也可以为package.json创建一个执行脚本build
-#  "scripts": {
-#    "build": "./node_modules/.bin/babel-node index.js"
-#  }
-# 输入简短的命令来执行
-# yarn build
+## 配置package.json的scripts.babel
+## "babel": "./node_modules/.bin/babel-node index.js"
+# yarn run babel
