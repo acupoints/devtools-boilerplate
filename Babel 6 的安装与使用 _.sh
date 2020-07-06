5729a772581d6f03654b0afb2ed9fe53895d2911
acupoints <67352324+acupoints@users.noreply.github.com>
2020-07-06 22:39:37 +0800

    Update Babel 6 的安装与使用 _.sh

--- Babel 6 的安装与使用 _.sh
+++ Babel 6 的安装与使用 _.sh
@@ -2,13 +2,19 @@ npm init -y
 npm install -D babel-cli babel-preset-es2015
 echo '{ "presets": ["es2015"] }' > .babelrc
 
-## 配置package.json的scripts.babel
-"babel": "./node_modules/.bin/babel-node index.js"
-npm run babel
+npm i -D uglify-es
 
-# yarn init -y
-# yarn add -D babel-cli babel-preset-es2015
-# echo '{ "presets": ["es2015"] }' > .babelrc
-## 配置package.json的scripts.babel
-## "babel": "./node_modules/.bin/babel-node index.js"
-# yarn run babel
+### package.json Scripts 属性添加以下脚本
+############################################################################
+    "babel": "./node_modules/.bin/babel browser.js -d build/",
+    "uglify": "./node_modules/.bin/uglifyjs build/browser.js -o build/browser.min.js",
+    "build": "npm run babel && npm run uglify"
+############################################################################
+
+## ES6 转译 ES5
+npm run babel
+## 删除ES5脚本中的空格
+npm run uglify
+
+## 也可以组合调用
+npm run build
