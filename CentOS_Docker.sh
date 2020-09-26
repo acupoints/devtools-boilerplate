b992f3290e1c64265f23e193c3affb95727faf42
acupoints <67352324+acupoints@users.noreply.github.com>
2020-09-26 08:17:06 +0800

    Update CentOS_Docker.sh

--- CentOS_Docker.sh
+++ CentOS_Docker.sh
@@ -1,7 +1,7 @@
 
-wget https://raw.githubusercontent.com/docker/compose/master/CHANGELOG.md
-compose_latest=$(grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' CHANGELOG.md | head -1)
-
+git clone https://github.com/docker/compose.git
+compose_latest=$(grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' compose/CHANGELOG.md | head -1)
+rm -rf compose/
 sudo curl -L "https://github.com/docker/compose/releases/download/$compose_latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
