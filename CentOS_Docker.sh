ebee25f423e8d894fe641602d6b41de8d80a99ed
acupoints <67352324+acupoints@users.noreply.github.com>
2020-09-26 08:47:50 +0800

    Update CentOS_Docker.sh

--- CentOS_Docker.sh
+++ CentOS_Docker.sh
@@ -2,9 +2,13 @@
 git clone https://github.com/docker/compose.git
 compose_latest=$(grep -Eo '[0-9]+\.[0-9]+\.[0-9]+' compose/CHANGELOG.md | head -1)
 rm -rf compose/
+
 ## You can also specify a comp version
 # compose_latest=1.27.3
 sudo curl -L "https://github.com/docker/compose/releases/download/$compose_latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+## You need to go over the wall when downloading, you can download from other places and copy to the current directory
+# sudo cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
