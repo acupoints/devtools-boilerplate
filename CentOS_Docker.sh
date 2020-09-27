25d5bfce884cddd08f7eabe726bc9b7bb5e0996d
acupoints <67352324+acupoints@users.noreply.github.com>
2020-09-27 09:58:59 +0800

    Update CentOS_Docker.sh

--- CentOS_Docker.sh
+++ CentOS_Docker.sh
@@ -1,13 +1,14 @@
 
-git clone git clone https://github.com/acupoints/devtools-boilerplate.git
+git clone https://github.com/acupoints/astrology-tools.git
 
-sudo cp docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+# sudo cp astrology-tools/docker-compose-1.27.3/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo cp astrology-tools/docker-compose-1.27.4/docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
 
-sudo cp docker-machine-Linux-x86_64 /usr/local/bin/docker-machine
+sudo cp astrology-tools/docker-machine-v0.16.2/docker-machine-Linux-x86_64 /usr/local/bin/docker-machine
 sudo chmod +x /usr/local/bin/docker-machine
 docker-machine version
 
-rm -rf devtools-boilerplate/
+rm -rf astrology-tools/
 
