99e0f73dfb48cf8374eb16d806b5b649a3644df1
acupoints <67352324+acupoints@users.noreply.github.com>
2020-10-19 14:55:20 +0800

    Update CentOS_BaseIDE.sh

--- CentOS_BaseIDE.sh
+++ CentOS_BaseIDE.sh
@@ -18,3 +18,43 @@ pyenv install --list
 pyenv install 3.7.6
 pyenv versions
 pyenv global 3.7.6
+
+## Install ruby
+### This method requires turning over the wall
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+git clone https://github.com/rvm/rvm.git
+cat rvm/binscripts/rvm-installer | sudo bash -s stable
+
+echo source /usr/local/rvm/scripts/rvm >> ~/.bashrc
+
+source ~/.bashrc
+rvm --version
+
+rvm list known
+rvm list known | grep 2.6
+rvm install 2.6.5
+rvm list
+rvm use 2.6.5 --default
+
+gem --version
+bundle --version
+gem install rails
+rails --version
+
+## Install nvm
+git clone https://github.com/nvm-sh/nvm.git
+cat nvm/install.sh | sudo bash
+
+source ~/.bashrc
+nvm --version
+
+nvm ls-remote
+nvm ls-remote | grep v12.16
+nvm install v12.16
+nvm ls
+nvm use v12.16
+
+node --version
+npm --version
+npm install -g yarn
+yarn --version
