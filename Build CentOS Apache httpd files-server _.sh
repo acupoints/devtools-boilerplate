bb3c1c69e4275e82bcbc7bcb7d6683033a3d14f2
acupoints <67352324+acupoints@users.noreply.github.com>
2020-10-29 17:53:42 +0800

    Update and rename Build CentOS file server _.sh to Build CentOS Apache httpd files-server _.sh

---
+++ Build CentOS Apache httpd files-server _.sh
@@ -0,0 +1,48 @@
+## Set up a CentOS file download server
+yum install httpd -y
+httpd -v
+service httpd start
+
+vim /etc/selinux/config
+SELINUX=disabled
+reboot
+
+su root
+yum install httpd -y
+httpd -version
+
+vim /etc/httpd/conf/httpd.conf
+### :set nu
+### :42<ENTER>
+ 42 Listen 9581
+
+mv /etc/httpd/conf.d/welcome.conf /etc/httpd/conf.d/welcome.conf.bak
+
+systemctl start httpd
+systemctl enable httpd
+
+mkdir -p /data/files
+ln -s /data/files  /var/www/html/
+echo "hello world" > /data/files/1.txt
+cp /etc/httpd/conf.d/welcome.conf /data/files/welcome.conf
+
+## 创建共享文件夹
+虚拟机|设置|共享文件夹|临时分配|添加共享文件夹
+**共享文件夹路径{C:\Users\literal\Desktop\CentOS-assets}
+**共享文件夹名称{CentOS-assets}(*选择了本地目录后会自动生成，此后挂载会用到这个名字)
+[-] 只读分配
+[-] 自动挂载
+[-] 固定分配
+
+(*以上三项无须勾选)
+(OK)
+
+## 挂载共享文件夹
+mount -t vboxsf CentOS-assets /data/file/
+## 取消挂载共享文件夹
+umount /data/file/
+
+## 防火墙可以不用设置
+systemctl stop firewalld
+systemctl disable firewalld
+
