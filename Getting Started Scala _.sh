e37d893465fc2a1165eec4a6f6273828cd08c2b4
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-03 14:00:41 +0800

    Create Getting Started Scala _.sh

---
+++ Getting Started Scala _.sh
@@ -0,0 +1,15 @@
+# Install Scala
+curl -O https://downloads.lightbend.com/scala/2.13.3/scala-2.13.3.tgz
+mkdir /usr/local/scala
+tar xzfv scala-2.13.3.tgz -C /usr/local/scala/
+
+source /etc/profile
+
+# Integrated Scala
+export SCALA_HOME=/usr/local/scala/scala-2.13.3
+export PATH=${SCALA_HOME}/bin:$PATH
+
+source /etc/profile
+
+scala -version
+
