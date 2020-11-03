72274715972439761bb348ca862382f39cc0de5b
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-03 12:12:23 +0800

    Create Clojure Getting Started _.sh

---
+++ Clojure Getting Started _.sh
@@ -0,0 +1,10 @@
+# Install Leiningen Clojure
+curl -O https://download.clojure.org/install/linux-install-1.10.1.727.sh
+chmod +x linux-install-1.10.1.727.sh
+sudo ./linux-install-1.10.1.727.sh
+
+# Install Leiningen
+git clone https://github.com/acupoints/astrology-tools.git
+chmod +x astrology-tools/Clojure/lein.sh
+sudo ./astrology-tools/Clojure/lein.sh
+
