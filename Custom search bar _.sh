8484168690156d5de71e0311c45c7c3165d56449
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-10 13:30:12 +0800

    Create Custom search bar _.sh

---
+++ Custom search bar _.sh
@@ -0,0 +1,32 @@
+<div class="ps-relative">
+    <input placeholder="Search…" value="" autocomplete="off" maxlength="240">
+    <svg class="s-input-icon" width="18" height="18"></svg>
+    <div class="s-popover"></div>
+</div>
+
+.top-bar .searchbar .s-input {
+    border-color: #673AB7;
+    background-color: #E91E63;
+    box-shadow: none;
+    color: #607D8B;
+    padding-left: 32px;
+}
+.s-input-icon {
+    position: absolute;
+    top: 50%;
+    right: .7em;
+    right: auto;
+    margin-top: -9px;
+    pointer-events: none;
+    left: .7em;
+}
+.RNNXgb:hover, .sbfc .RNNXgb {
+    box-shadow: 0 1px 6px rgba(32,33,36,.28);
+    border-color: rgba(223,225,229,0);
+}
+.emcav .RNNXgb {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    border-color: rgba(223,225,229,0);
+    box-shadow: 0 1px 6px rgba(32,33,36,.28);
+}
