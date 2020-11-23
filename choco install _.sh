fe11a8995a1ef704321d1bef1ce65b8be5bad63b
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-24 04:08:08 +0800

    Delete choco install _.sh

--- choco install _.sh
+++
@@ -1,7 +0,0 @@
-# Install PostgreSQL 12.2
-# cinst postgresql12 --params '/Password:Umph2014' --version=12.2
-# refreshenv
-
-##
-choco install postgresql12 --version=12.3 --params '/Password:Umph2014'
-refreshenv
