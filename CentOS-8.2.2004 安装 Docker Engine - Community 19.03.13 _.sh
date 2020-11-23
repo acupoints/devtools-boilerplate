a4b291be990673a5006d0d6d88083f233cc6b210
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-23 13:37:36 +0800

    Create CentOS-8.2.2004 安装 Docker Engine - Community 19.03.13 _.sh

---
+++ CentOS-8.2.2004 安装 Docker Engine - Community 19.03.13 _.sh
@@ -0,0 +1,10 @@
+## 卸载早期安装的版本
+[root@localhost gullies]# yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+## 安装新版本
+[root@localhost gullies]# yum install -y yum-utils device-mapper-persistent-data lvm2
+[root@localhost gullies]# yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+[root@localhost gullies]# yum install -y docker-ce docker-ce-cli containerd.io --nobest
+[root@localhost gullies]# systemctl start docker.service
+[root@localhost gullies]# systemctl enable docker.service
+[root@localhost gullies]# docker --version
