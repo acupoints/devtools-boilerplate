5d2616092b2e5967a7d73ce8e2ad28df87108a84
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-24 07:52:03 +0800

    Update CentOS-8.2.2004 安装 Docker Engine - Community 19.03.13 _.sh

--- CentOS-8.2.2004 安装 Docker Engine - Community 19.03.13 _.sh
+++ CentOS-8.2.2004 安装 Docker Engine - Community 19.03.13 _.sh
@@ -1,24 +1,17 @@
 ## 卸载早期安装的版本
-[root@localhost gullies]# yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
-
-## 安装新版本
-[root@localhost gullies]# yum install -y yum-utils device-mapper-persistent-data lvm2
-[root@localhost gullies]# yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-[root@localhost gullies]# yum install -y docker-ce docker-ce-cli containerd.io --nobest
-[root@localhost gullies]# systemctl start docker.service
-[root@localhost gullies]# systemctl enable docker.service
-[root@localhost gullies]# docker --version
-
+yum list installed | grep docker
+yum remove -y containerd.io.x86_64
+yum remove -y docker-ce-cli.x86_64
 
 ## 去掉了提示符，方便整体复制，快速安装新版本
 ###########################################################################
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-# yum install -y docker-ce docker-ce-cli containerd.io --nobest
 yum -y install docker-ce --nobest
 systemctl start docker.service
 systemctl enable docker.service
 docker --version
+
 
 ## 常用的数据库镜像拉取
 # docker pull postgres
