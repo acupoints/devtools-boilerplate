df764984cb9a68c3225e111ccbfc23283f287429
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-24 04:17:03 +0800

    Update 常用的 choco install _.sh

--- 常用的 choco install _.sh
+++ 常用的 choco install _.sh
@@ -1,3 +1,11 @@
+PS Admin> curl -O https://chocolatey.org/install.ps1
+PS Admin> iex .\install.ps1
+PS Admin> choco --version
+## Win7可以考虑升级到PowerShell5.1之后，再进行安装
+## 安装NDP452-KB2901907-x86-x64-AllOS-ENU.exe
+## 解压Win7AndW2K8R2-KB3191566-x64.zip之后，安装Win7AndW2K8R2-KB3191566-x64.msu
+###
+
 choco install vagrant
 
 # Install PostgreSQL 12.3
