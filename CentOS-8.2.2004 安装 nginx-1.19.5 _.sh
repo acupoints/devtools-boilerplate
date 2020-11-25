4eadfc79c92465107a16a1aabd0a78198c85437c
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-26 00:06:29 +0800

    Update CentOS-8.2.2004 安装 nginx-1.19.5 _.sh

--- CentOS-8.2.2004 安装 nginx-1.19.5 _.sh
+++ CentOS-8.2.2004 安装 nginx-1.19.5 _.sh
@@ -56,3 +56,91 @@ yum remove nginx
 nginx: /usr/local/nginx
 [root@localhost nginx]#
 
+### 补充
+## nginx 配置https域名访问
+#####################################################
+C:\Users\Administrator\Desktop
+λ scp -r 4803322_fy1m.cn_nginx.zip gullies@192.168.56.101:/usr/local/nginx/fy1m_cert
+gullies@192.168.56.101's password:
+scp: /usr/local/nginx/fy1m_cert/4803322_fy1m.cn_nginx.zip: Permission denied
+
+## 见到 Permission denied 说明目标文件夹没有写入权限，如何添加权限往下接着读
+----------------------------------------------------
+[root@localhost nginx]# mkdir fy1m_cert
+[root@localhost nginx]# ls
+client_body_temp  conf  fastcgi_temp  fy1m_cert  html  logs  nginx.conf.bak  proxy_temp  sbin  scgi_temp  uwsgi_temp
+[root@localhost nginx]# pwd
+/usr/local/nginx
+[root@localhost nginx]# ls
+client_body_temp  conf  fastcgi_temp  fy1m_cert  html  logs  nginx.conf.bak  proxy_temp  sbin  scgi_temp  uwsgi_temp
+[root@localhost nginx]# chmod 777 fy1m_cert/
+[root@localhost nginx]#
+
+## 给目标文件夹添加好权限，接着拷贝证书，可以看出这次成功上传
+----------------------------------------------------
+C:\Users\Administrator\Desktop
+λ scp -r 4803322_fy1m.cn_nginx.zip gullies@192.168.56.101:/usr/local/nginx/fy1m_cert
+gullies@192.168.56.101's password:
+4803322_fy1m.cn_nginx.zip                                                                      100% 4156   831.7KB/s   00:00
+
+C:\Users\Administrator\Desktop
+## 回到服务器端，解压证书
+----------------------------------------------------
+[root@localhost nginx]# ls
+client_body_temp  conf  fastcgi_temp  fy1m_cert  html  logs  nginx.conf.bak  proxy_temp  sbin  scgi_temp  uwsgi_temp
+[root@localhost nginx]# ls fy1m_cert/
+4803322_fy1m.cn_nginx.zip
+[root@localhost nginx]# unzip fy1m_cert/4803322_fy1m.cn_nginx.zip -d fy1m_cert/
+Archive:  fy1m_cert/4803322_fy1m.cn_nginx.zip
+Aliyun Certificate Download
+  inflating: fy1m_cert/4803322_fy1m.cn.pem
+  inflating: fy1m_cert/4803322_fy1m.cn.key
+[root@localhost nginx]# ls fy1m_cert/
+4803322_fy1m.cn.key  4803322_fy1m.cn_nginx.zip  4803322_fy1m.cn.pem
+[root@localhost nginx]#
+
+### 证书有了，就该配置nginx的https域名访问了。
+----------------------------------------------------
+    #gzip  on;                                  
+                                                
+    server {                                    
+        listen       80;                        
+----------------------------------------------------配置以下2行
+        #server_name  localhost;
+        server_name  fy1m.cn www.fy1m.cn;                 
+        return      301 https://$server_name$request_uri;
+        #charset koi8-r;                        
+                                                
+        #access_log  logs/host.access.log  main;
+
+### 配置证书
+###############################################################
+    # HTTPS server
+    #
+    server {
+        listen       443 ssl;
+----------------------------------------------------配置以下3行
+        #server_name  localhost;
+        server_name  fy1m.cn www.fy1m.cn;
+
+        #ssl_certificate      cert.pem;
+        ssl_certificate      ../fy1m_cert/4803322_fy1m.cn.pem;
+        #ssl_certificate_key  cert.key;
+        ssl_certificate_key  ../fy1m_cert/4803322_fy1m.cn.key;
+
+        ssl_session_cache    shared:SSL:1m;
+        ssl_session_timeout  5m;
+
+----------------------------------------------------配置以下1行
+	    ssl_protocols TLSv1 TLSv1.1 TLSv1.2;
+
+        ssl_ciphers  HIGH:!aNULL:!MD5;
+        ssl_prefer_server_ciphers  on;
+
+        location / {
+            root   html;
+            index  index.html index.htm;
+        }
+    }
+    
+    
