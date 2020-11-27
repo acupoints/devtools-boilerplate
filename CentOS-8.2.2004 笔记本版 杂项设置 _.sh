ab48bc7a1bb3953d663167d7af98af1bb1328179
acupoints <67352324+acupoints@users.noreply.github.com>
2020-11-28 02:34:25 +0800

    Update CentOS-8.2.2004 笔记本版 杂项设置 _.sh

--- CentOS-8.2.2004 笔记本版 杂项设置 _.sh
+++ CentOS-8.2.2004 笔记本版 杂项设置 _.sh
@@ -1,4 +1,5 @@
-## 笔记本合上盖子不休眠
+### 笔记本合上盖子不休眠
+#######################################################################
 [root@localhost gullies]# vim /etc/systemd/logind.conf
 :/Lid
 :set nu
@@ -6,5 +7,14 @@
 :noh
 HandleLidSwitch=lock
 
+### 重启此服务太慢，推荐 reboot 重启
 [root@localhost gullies]# systemctl restart systemd-logind
 
+### Win10 hosts not found
+#######################################################################
+cmd admin> for /f %P in ('dir %windir%\WinSxS\hosts /b /s') do copy %P %windir%\System32\drivers\etc & echo %P & Notepad %P
+
+### 墙内访问 GitHub 用户原生文件
+#######################################################################
+echo ## The GitHub configuration file cannot be downloaded >> /etc/hosts
+echo 199.232.68.133 raw.githubusercontent.com >> /etc/hosts
