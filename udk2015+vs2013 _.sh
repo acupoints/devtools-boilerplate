d06cdd00dec9c7daa4c247dcd298e7e122df0420
acupoints <67352324+acupoints@users.noreply.github.com>
2021-02-18 15:37:59 +0800

    Update udk2015+vs2013 _.sh

--- udk2015+vs2013 _.sh
+++ udk2015+vs2013 _.sh
@@ -8,9 +8,11 @@ cd openssl-1.0.2d
 patch -p0 -i ..\EDKII_openssl-1.0.2d.patch
 cd ..
 Install.cmd
+
 ### Run the simulator
 set PYTHON_HOME=C:\Python37
 edksetup.bat --nt32
+
 build -t VS2013x86
 build run
 
