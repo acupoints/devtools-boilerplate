5d2ce474e7a8ccf011a8c78bf84077d24abbe79d
acupoints <67352324+acupoints@users.noreply.github.com>
2021-03-09 12:38:52 +0800

    Create Vue2.x _.sh

---
+++ Vue2.x _.sh
@@ -0,0 +1,14 @@
+$ node -v
+v12.16.2
+
+$ npm -v
+6.14.4
+
+$ npm install vue -g
++ vue@2.6.12
+added 1 package from 1 contributor in 2.289s
+
+## 可以不用安装，安装插件在打开的源码文件上右键 Open with Live Server
+$ npm install live-server -g
++ live-server@1.2.1
+added 194 packages from 149 contributors in 19.941s
